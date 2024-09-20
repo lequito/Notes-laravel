8342f056f1f5df79d4cc539484c4344adce5ed17
Lekito <alexdossantosdias1212@gmail.com>
2024-09-19 22:10:40 -0300

    004

--- notes/app/Http/Controllers/MainController.php
+++ notes/app/Http/Controllers/MainController.php
@@ -6,16 +6,5 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function index()
-    {
-       return view('main');
-    }
-
-    public function page1(){
-       return view('page1');
-    }
-
-    public function page2(){
-       return view('page2');
-    }
+   
 }
--- notes/routes/web.php
+++ notes/routes/web.php
@@ -3,14 +3,3 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo "Hello world";
-});
-
-Route::get('/aboute', function(){
-    echo "Aboute us";
-});
-
-Route::get('/main', [MainController::class, 'index']);
-Route::get('/page1', [MainController::class, 'page1']);
-Route::get('/page2', [MainController::class, 'page2']);
