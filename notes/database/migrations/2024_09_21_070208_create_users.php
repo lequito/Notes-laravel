3210c9cc7e52da0f385a68e730b0c3ae109ab323
Lekito <alexdossantosdias1212@gmail.com>
2024-09-21 15:05:59 -0300

    010 - seeders

--- notes/database/migrations/2024_09_21_070208_create_users.php
+++ notes/database/migrations/2024_09_21_070208_create_users.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id()->autoIncrement();
             $table->string('username', 50)->nullable();
-            $table->string('password', 50)->nullable();
+            $table->string('password', 250)->nullable();
             $table->dateTime('last_login')->nullable();
             $table->timestamps();
             $table->softDeletes();
