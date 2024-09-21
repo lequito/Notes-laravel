b790182f51fceb165598ccd04003195893e32bc8
Lekito <alexdossantosdias1212@gmail.com>
2024-09-21 01:42:46 -0300

    006

--- notes/app/Http/Controllers/AuthController.php
+++ notes/app/Http/Controllers/AuthController.php
@@ -9,6 +9,22 @@ class AuthController extends Controller{
         return view('login');
     }
 
+    public function loginSubmit(Request $req){
+        //form validate
+        $req->validate(
+            [
+                'text_username' => 'required',
+                'text_password' => 'required'
+            ]
+        );
+
+        $username = $req->input('text_username');
+        $username = $req->input('text_password');
+
+        echo "ok";
+    }
+    
+
     public function logout(){
         echo "Logout";
     }
