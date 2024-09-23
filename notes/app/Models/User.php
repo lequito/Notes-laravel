b25a69279a2a0d0bad301e4ddf7823faf4ef0eac
Lekito <alexdossantosdias1212@gmail.com>
2024-09-22 23:46:34 -0300

    018 - Create Model Notes and relationship whit Model User

--- notes/app/Models/User.php
+++ notes/app/Models/User.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
-{
-    
+class User extends Model{
+    public function notes(){
+        return $this->hasMany(Note::class);
+    }
 }
